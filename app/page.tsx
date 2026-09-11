f914f739d3f0e25856348d2c9002be6c8d0c7e5b
monsterdent2531-debug <monsterdent2531@gmail.com>
2026-09-12 01:47:55 +0700

    Add captain selector

--- app/page.tsx
+++ app/page.tsx
@@ -1356,18 +1356,18 @@ export default function Home() {
                       "
 
                       style={{
-                        width: "22%",
+                        width: "15%",
                         aspectRatio:
                           "1 / 1",
                         right: "10%",
                         top: "30%",
                         backgroundColor:
                           "#facc15",
-                        color: "#000000",
+                        color: "#020000",
                         fontSize:
-                          "clamp(18px, 1.8vw, 18px)",
+                          "clamp(10px, 1.8vw, 18px)",
                         boxShadow:
-                          "0 2px 8px rgba(245, 241, 241, 0.45)",
+                          "0 2px 8px rgba(0,0,0,0.45)",
                       }}
                     >
                       C
